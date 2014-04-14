b25cb8996785ced26a55098bc9b93e971a42c5f5
Dooptroop <mikerdupree@gmail.com>
2014-04-13 18:14:51 -0700

    Update*
    UPDATE

--- SortCanvas.java
+++ SortCanvas.java
@@ -6,26 +6,37 @@ import java.awt.Dimension;
 import java.awt.Graphics;
 import java.awt.GraphicsConfiguration;
 
-public class SortCanvas extends Canvas {
+public class SortCanvas extends Canvas implements Runnable {
 
 	private Graphics g;
 	private int bars = 15;
 	private int[] barsArray;
+	private int space=2;
+	private int sortType;
+	private int delay;
 	
 	public SortCanvas(int w, int h) {
+		setBackground(Color.BLACK);
 		setPreferredSize(new Dimension(w, h));
-		createBarArray();
 	}
-
+	public void sort(int sortType, int delay) {
+		this.sortType = sortType;
+		this.delay = delay;
+		Thread thread = new Thread(this);
+		thread.start();
+		
+	}
+	
 	public void paint(Graphics g){
 		super.paint(g);
+		createBarArray();
 		paintBars(g,bars);
 	}
 
 	private void paintBars(Graphics g, int bars) {
-		g.setColor(Color.RED);
+		g.setColor(Color.GREEN);
 		for(int x=5, index=0; x<getWidth(); x+=getWidth()/bars, index++){
-			g.fillRect(x, getHeight()-barsArray[index], getWidth()/bars, getHeight());
+				g.fillRect(x, getHeight()-barsArray[index], getWidth()/bars-space, getHeight());
 		}
 		
 	}
@@ -33,6 +44,35 @@ public class SortCanvas extends Canvas {
 		barsArray = new int[bars];
 		for(int i=0; i<barsArray.length; i++){
 			barsArray[i]=(int)(Math.random()*getHeight());
+			System.out.println(barsArray[i]);
 		}
 	}
+
+	@Override
+	public void run() {
+		
+		repaint();//repaints the screen
+		try { // try to sleep. if not throw exception to stack trace.
+			Thread.sleep(17);// try sleep for 17 milliseconds
+		} catch (InterruptedException e) {
+			e.printStackTrace();// print to stack
+		}
+		
+		
+	}
+	public void setBars(int bars) {
+		this.bars=bars;
+	}
+	public int getSortType() {
+		return sortType;
+		
+	}
+	public int getSpeed() {
+		return delay;
+	}
+	public void reset() {
+		createBarArray();
+		repaint();
+	}
+	
 }
--- SortPanel.java
+++ SortPanel.java
@@ -1,20 +1,91 @@
 package SortingAlgorithms;
 
+import java.awt.BorderLayout;
 import java.awt.LayoutManager;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-import javax.swing.JFrame;
-import javax.swing.JPanel;
+import javax.swing.*;
 
 @SuppressWarnings("serial")
-public class SortPanel extends JPanel {
-
+public class SortPanel extends JPanel implements ActionListener {
+	
+	// Radio Buttons for Algorith selection.
+	private JPanel algPanel = new JPanel();
+	private ButtonGroup algBtnGroup = new ButtonGroup();
+	private JRadioButton bubble = new JRadioButton("BubbleSort", true);
+	private JRadioButton selection = new JRadioButton("SelectionSort");
+	private JRadioButton insertion = new JRadioButton("InsertionSort");
+	
+	// Radio Buttons for speed selection.
+	private JPanel spdPanel = new JPanel();
+	private ButtonGroup spdBtnGroup = new ButtonGroup();
+	private JRadioButton slow = new JRadioButton("BubbleSort", true);
+	private JRadioButton medium = new JRadioButton("SelectionSort");
+	private JRadioButton fast = new JRadioButton("InsertionSort");
+	
+	// buttons for Reset, and Start
+	private JPanel funcPanel = new JPanel();
+	protected JButton reset = new JButton("Reset");
+	protected JButton start = new JButton("start");
+	
+	SortCanvas canvas = new SortCanvas(500,300);
+	private int bars;
 	
 	public SortPanel() {
+		//add to button groups
+		algBtnGroup.add(bubble);
+		algBtnGroup.add(selection);
+		algBtnGroup.add(insertion);
+		spdBtnGroup.add(slow);
+		spdBtnGroup.add(medium);
+		spdBtnGroup.add(fast);
 		
-		SortCanvas canvas = new SortCanvas(500,300);
-		add(canvas);
+		
+		// add to selection panel
+		algPanel.add(bubble);
+		algPanel.add(selection);
+		algPanel.add(insertion);
+		spdPanel.add(slow);
+		spdPanel.add(medium);
+		spdPanel.add(fast);
+		
+		
+		funcPanel.add(reset);
+		funcPanel.add(start);
+		
+		
+		// add to SortPanel
+		add(algPanel, BorderLayout.NORTH);
+		add(canvas, BorderLayout.CENTER);
+		add(spdPanel, BorderLayout.EAST);
+		add(funcPanel, BorderLayout.SOUTH);
+		
+		
+		// add Action Listeners
+		reset.addActionListener(this);
+		start.addActionListener(this);
+		
+		
 		
 	}
+	public void start(){
+		//int bars = Integer.parseInt(textfield.getText());
+	    canvas.setBars(bars);
+	    canvas.sort(getSortType(), getDelaySpeed());
+	    start.setEnabled(false);
+	    reset.setEnabled(false);
+	}
+	private int getDelaySpeed() {return canvas.getSpeed();}
+	private int getSortType() {return canvas.getSortType();}
+	
+	public void actionPerformed(ActionEvent action) {
+		if (action.getSource() == start) {start();}
+		else if(action.getSource() == reset) {canvas.reset();}
+		
+	}
+	
+	
 	// Main method
     public static void main(String[] args) {
 	JFrame myApp = new JFrame("Sorting GUI");
@@ -22,9 +93,10 @@ public class SortPanel extends JPanel {
 	myApp.add(myPanel);
 	myApp.pack();
 	myApp.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-	myApp.setSize(635, 385);
+	myApp.setSize(635, 445);
 	myApp.setVisible(true);
 	myApp.setResizable(false);
 	myApp.setLocationRelativeTo(null);
     } // end Main
+	
 }
